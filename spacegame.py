f99fb3add78d8f50913ff25cde3b8b9b34a3df34
Gautam Jagdhish <gautamjagdhish@gmail.com>
2020-08-08 18:56:05 +0530

    multiple enemies working

--- spacegame.py
+++ spacegame.py
@@ -1,14 +1,16 @@
 import pygame as pg
 import random as r
+import math as m
 
-screenw = 800
-screenh = 600
 pg.init()
-screen = pg.display.set_mode((screenw, screenh))
-
 pg.display.set_caption("Space Invaders")
 icon = pg.image.load('media/icon.png')
 pg.display.set_icon(icon)
+
+#screen dim
+screenw = 800
+screenh = 600
+screen = pg.display.set_mode((screenw, screenh))
 
 #player
 playerImg = pg.image.load('media/player.png')
@@ -17,22 +19,18 @@ playerImgh = playerImg.get_size()[1]
 playerX = 0.5*screenw - playerImgw//2
 playerY = 0.8*screenh
 deltaPlayerX = 0
-speed = 1 # control player speed
-
-def player(x, y) :
-    screen.blit(playerImg, (int(x), int(y)))
+speed = 1.5 # control player speed
 
 #enemy
+
 enemyImg = pg.image.load('media/enemy.png')
 enemyImgw = enemyImg.get_size()[0]
 enemyImgh = enemyImg.get_size()[1]
-enemyX = r.randint(0, screenw - enemyImgw)
-enemyY = r.randint(0.1*screenh, 0.3*screenh)
-deltaEnemyX = speed*0.8   # control enemy speed
 deltaEnemyY = enemyImgh/3
-
-def enemy(x, y) :
-    screen.blit(enemyImg, (int(x), int(y)))
+enemyN = 25 #number of enemies
+enemyX = [0]*enemyN
+enemyY = [0]*enemyN
+deltaEnemyX = [speed*0.8]*enemyN   # control enemy speed
 
 #bg
 bgImg = pg.image.load('media/bg.jpg')
@@ -45,15 +43,51 @@ bulletImgh = bulletImg.get_size()[1]
 bulletImg = pg.transform.scale(bulletImg, (int(0.5*bulletImgw), int(0.5*bulletImgh)))
 bulletImgw = bulletImg.get_size()[0]    #update after transform
 bulletImgh = bulletImg.get_size()[1]
-bulletX = playerX + playerImgw/2 - bulletImgw/2
-bulletY = playerY
-deltaBulletY = 2 # control bullet speed
-bulletState = "ready" #bullet is not fired yet 
+deltaBulletY = 4 # control bullet speed
+bulletX = bulletY = 0
+bulletState = "ready" #bullet is not fired yet
+
+#blit functions
+def player(x, y) :
+    screen.blit(playerImg, (int(x), int(y)))
+
+def enemy(x, y) :
+    screen.blit(enemyImg, (int(x), int(y)))
 
 def bullet(x, y) :
     screen.blit(bulletImg, (int(x), int(y)))
 
+#sets random enemy coordinates
+def resetEnemy(i) :  
+    global enemyX
+    global enemyY
+    global deltaEnemyX
+    enemyX[i] = r.randint(0, screenw - enemyImgw)
+    enemyY[i] = r.randint(0.1*screenh, 0.3*screenh)
+    deltaEnemyX[i] = r.uniform(0.5, 1)*speed
+
+#resets bullet coordinates
+def resetBullet() : 
+    global bulletX
+    global bulletY
+    global bulletState
+    bulletX = playerX + playerImgw/2 - bulletImgw/2
+    bulletY = playerY
+    bulletState = "ready"
+
+#detect collision
+def isCollision(enemyX, enemyY, bulletX, bulletY) :
+    if enemyY + enemyImgh >= bulletY:
+        bulletXCen = bulletX + bulletImgw/2
+        if bulletXCen >= enemyX and bulletXCen <= enemyX + enemyImgw :
+            return True
+
+for i in range(enemyN) :
+    resetEnemy(i)
+resetBullet()
+score = 0
 running = True
+
 while running :
     # load bg
     screen.blit(bgImg, (0, 0))
@@ -72,9 +106,8 @@ while running :
                 deltaPlayerX = 1
             #shoot bullet
             if event.key == pg.K_SPACE and bulletState == "ready" :
+                resetBullet()
                 bulletState = "fire"
-                bulletX = playerX + playerImgw/2 - bulletImgw/2
-                bulletY = playerY - bulletImgh/2
 
         if event.type == pg.KEYUP :
             if event.key == pg.K_a or event.key == pg.K_d :
@@ -86,18 +119,26 @@ while running :
         playerX = screenw - playerImgw
     elif playerX < 0 :
         playerX = 0
+    player(playerX, playerY)
 
     # enemy movement
-    enemyX += deltaEnemyX * speed
-    if enemyX > screenw - playerImgw :
-        enemyX = screenw - playerImgw
-        deltaEnemyX = -deltaEnemyX
-        enemyY += deltaEnemyY
-    elif enemyX < 0 :
-        enemyX = 0
-        deltaEnemyX = -deltaEnemyX
-        enemyY += deltaEnemyY
-    
+    for i in range(enemyN) :
+        enemyX[i] += deltaEnemyX[i]
+        if enemyX[i] > screenw - playerImgw :
+            enemyX[i] = screenw - playerImgw
+            deltaEnemyX[i] = -deltaEnemyX[i]
+            enemyY[i] += deltaEnemyY
+        elif enemyX[i] < 0 :
+            enemyX[i] = 0
+            deltaEnemyX[i] = -deltaEnemyX[i]
+            enemyY[i] += deltaEnemyY
+        if isCollision(enemyX[i], enemyY[i], bulletX, bulletY) :
+            resetBullet()
+            resetEnemy(i)
+            score += 1
+            print(score)
+        enemy(enemyX[i], enemyY[i])
+        
     #bullet movement
     if bulletState == "fire" :
         bulletY -= deltaBulletY
@@ -105,9 +146,5 @@ while running :
     if bulletY < -bulletImgh :
         bulletY = playerY - bulletImgh/2
         bulletState = "ready"
-
-    # set positions
-    player(playerX, playerY)
-    enemy(enemyX, enemyY)
 
     pg.display.update()
